accf340450b6920448ce6006b6f0a612800647e8
faizkhan005 <67471969+faizkhan005@users.noreply.github.com>
2022-07-27 23:24:37 +0530

    Create 024 missingNumber.java

---
+++ leetcode Questions/024 missingNumber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int j,i=0;
+        while(i<nums.length){
+            
+            if(nums[i]<nums.length && nums[i]!=i)
+                swap(nums,i,nums[i]);
+            else 
+                i++;
+                
+        }
+        for( j =0;j<nums.length;j++){
+            if(nums[j]!=j)
+               break;
+        }if(j==nums.length-1)
+            return j++;
+        return j;
+    }
+    public void swap(int[] arr, int first, int last){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+}

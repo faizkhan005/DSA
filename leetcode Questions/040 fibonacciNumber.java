d9f714f5974897c290178d91116290e3fe2c0863
faizkhan005 <67471969+faizkhan005@users.noreply.github.com>
2022-08-24 12:49:23 +0530

    Update 040 fibonacciNumber.java

--- leetcode Questions/040 fibonacciNumber.java
+++ leetcode Questions/040 fibonacciNumber.java
@@ -4,3 +4,10 @@ class Solution {
         return fib(n-1)+fib(n-2);
     }
 }
+
+// best solution use long isntead of int for large number
+class Solution {
+    public int fib(int n) {
+       return (int)((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
+    }
+}
